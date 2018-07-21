e09dd3f2e6e210e08502547291132ffded5b4a8a
ZZhuYYanjiang <2067484387@qq.com>
2018-07-21 13:50:54 +0800

    add backbutton for settingpage back to main

--- StickyNotes/SettingPage.xaml.cs
+++ StickyNotes/SettingPage.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
@@ -30,6 +31,13 @@ namespace StickyNotes
             redSlider.Value = 128;
             greenSlider.Value = 128;
             blueSlider.Value = 128;
+        }
+        private void BackButton_OnClick(object sender, RoutedEventArgs e) {
+            OnBackRequested();
+        }
+
+        private void OnBackRequested() {
+            Frame.GoBack(new DrillInNavigationTransitionInfo());
         }
 
         private void Cstyle_Loaded(object sender, RoutedEventArgs e) {
