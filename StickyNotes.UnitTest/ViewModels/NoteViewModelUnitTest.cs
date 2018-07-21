728e99e89060014dfe8e31189ae415627345446d
HZZW <3590139958+HZZW@users.noreply.github.com>
2018-07-21 14:42:42 +0800

    NoteViewModel连接时间提醒,单元测试更新

--- StickyNotes.UnitTest/ViewModels/NoteViewModelUnitTest.cs
+++ StickyNotes.UnitTest/ViewModels/NoteViewModelUnitTest.cs
@@ -112,7 +112,7 @@ namespace StickyNotes.UnitTest.ViewModels
             noteSaveList.Add(new Note() { Author = "LwwWG", Content = "it is a easy content four", Title = "title four", NotificationDateTime = new DateTime(2018, 12, 10) });
             noteViewModel.PushCommand.Execute(noteSaveList);
             //修改
-            noteViewModel.SetDateTimeCommand.Execute(new KeyValuePair<Note,DateTime> (noteViewModel.Note[0], new DateTime(2017, 12, 10)));
+            noteViewModel.SetDateTimeCommand.Execute(new KeyValuePair<Note, DateTime> (noteViewModel.Note[0], new DateTime(2017, 12, 10)));
             noteViewModel.SetDateTimeCommand.Execute(new KeyValuePair<Note, DateTime>(noteViewModel.Note[1], new DateTime(2017, 11, 10)));
             noteViewModel.SetDateTimeCommand.Execute(new KeyValuePair<Note, DateTime>(noteViewModel.Note[2], new DateTime(2017, 10, 10)));
             noteViewModel.SetDateTimeCommand.Execute(new KeyValuePair<Note, DateTime>(noteViewModel.Note[3], new DateTime(2017, 9,  10)));
@@ -132,7 +132,6 @@ namespace StickyNotes.UnitTest.ViewModels
             Assert.AreEqual(noteViewModel.Note[2].NotificationDateTime,DateTime.MinValue);
             Assert.AreEqual(noteViewModel.Note[3].NotificationDateTime,DateTime.MinValue);
         }
-
         [TestMethod]
         public void TestSetTagCommand()
         {
@@ -174,10 +173,55 @@ namespace StickyNotes.UnitTest.ViewModels
         }
 
         [TestMethod]
+        public void TestSetAndCancelNotification()
+        {
+            //设置数据
+            var noteViewModel = new NoteViewModel();
+            var noteSaveList = new List<Note>();
+            noteSaveList.Add(new Note() { Author = "LwwWG", Content = "it is a easy content one"  , Title = "title one"  ,  NotificationDateTime = new DateTime(2018, 9, 10),  Tag = "第一组" });
+            noteSaveList.Add(new Note() { Author = "LwwWG", Content = "it is a easy content two"  , Title = "title two"  ,  NotificationDateTime = new DateTime(2018, 10, 10), Tag = "第一组" });
+            noteSaveList.Add(new Note() { Author = "LwwWG", Content = "it is a easy content three", Title = "title three",  NotificationDateTime = new DateTime(2018, 11, 10), Tag = "第一组" });
+            noteSaveList.Add(new Note() { Author = "LwwWG", Content = "it is a easy content four" , Title = "title four" ,  NotificationDateTime = new DateTime(2018, 12, 10), Tag = "第二组" });
+            noteSaveList.Add(new Note() { Author = "LwwWG", Content = "it is a easy content four" , Title = "title four" ,  NotificationDateTime = new DateTime(2018, 12, 10), Tag = "第二组" });
+            noteSaveList.Add(new Note() { Author = "LwwWG", Content = "it is a easy content five" , Title = "title five" ,  NotificationDateTime = new DateTime(2018, 12, 10), Tag = "第二组" });
+            noteSaveList.Add(new Note() { Author = "LwwWG", Content = "it is a easy content six " , Title = "title six " ,  NotificationDateTime = new DateTime(2018, 12, 10), Tag = "第二组" });
+            noteViewModel.PushCommand.Execute(noteSaveList);
+            //SetDateTimeCommand
+            noteViewModel.SetDateTimeCommand.Execute(new KeyValuePair<Note,DateTime>(noteSaveList[0],noteSaveList[0].NotificationDateTime));
+            noteViewModel.SetDateTimeCommand.Execute(new KeyValuePair<Note,DateTime>(noteSaveList[1],noteSaveList[1].NotificationDateTime));
+            noteViewModel.SetDateTimeCommand.Execute(new KeyValuePair<Note,DateTime>(noteSaveList[2],noteSaveList[2].NotificationDateTime));
+            noteViewModel.SetDateTimeCommand.Execute(new KeyValuePair<Note,DateTime>(noteSaveList[3],noteSaveList[3].NotificationDateTime));
+            noteViewModel.SetDateTimeCommand.Execute(new KeyValuePair<Note,DateTime>(noteSaveList[4],noteSaveList[4].NotificationDateTime));
+            noteViewModel.SetDateTimeCommand.Execute(new KeyValuePair<Note,DateTime>(noteSaveList[5],noteSaveList[5].NotificationDateTime));
+            noteViewModel.SetDateTimeCommand.Execute(new KeyValuePair<Note,DateTime>(noteSaveList[6],noteSaveList[6].NotificationDateTime));
+            //检查
+            Assert.AreEqual(true, Notification.Instance.Show().Contains(noteSaveList[0].ID.ToString()));
+            Assert.AreEqual(true, Notification.Instance.Show().Contains(noteSaveList[1].ID.ToString()));
+            Assert.AreEqual(true, Notification.Instance.Show().Contains(noteSaveList[2].ID.ToString()));
+            Assert.AreEqual(true, Notification.Instance.Show().Contains(noteSaveList[3].ID.ToString()));
+            Assert.AreEqual(true, Notification.Instance.Show().Contains(noteSaveList[4].ID.ToString()));
+            Assert.AreEqual(true, Notification.Instance.Show().Contains(noteSaveList[5].ID.ToString()));
+            Assert.AreEqual(true, Notification.Instance.Show().Contains(noteSaveList[6].ID.ToString()));
+            //CancelDateTimeCommand
+            noteViewModel.CancelDateTimeCommand.Execute(noteSaveList[0]);
+            noteViewModel.CancelDateTimeCommand.Execute(noteSaveList[1]);
+            noteViewModel.CancelDateTimeCommand.Execute(noteSaveList[2]);
+            //检查
+            Assert.AreEqual(true, !Notification.Instance.Show().Contains(noteSaveList[0].ID.ToString()));
+            Assert.AreEqual(true, !Notification.Instance.Show().Contains(noteSaveList[1].ID.ToString()));
+            Assert.AreEqual(true, !Notification.Instance.Show().Contains(noteSaveList[2].ID.ToString()));
+            Assert.AreEqual(true,  Notification.Instance.Show().Contains(noteSaveList[3].ID.ToString()));
+            Assert.AreEqual(true,  Notification.Instance.Show().Contains(noteSaveList[4].ID.ToString()));
+            Assert.AreEqual(true,  Notification.Instance.Show().Contains(noteSaveList[5].ID.ToString()));
+            Assert.AreEqual(true,  Notification.Instance.Show().Contains(noteSaveList[6].ID.ToString()));
+        }
+        
+        [TestMethod]
         public void TestAllCommandTogether()
         {
             
         }
+
         /// <summary>
         /// 执行多次命令
         /// </summary>
