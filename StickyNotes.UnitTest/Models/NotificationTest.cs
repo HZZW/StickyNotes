1d2b9c71096d4b920fb229edff9a468b70c182af
hohochicken <1294332685@qq.com>
2018-07-27 14:20:23 +0800

    添加提醒测试,但是跑不起来

---
+++ StickyNotes.UnitTest/Models/NotificationTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StickyNotes.Models;
+
+namespace StickyNotes.UnitTest.Models
+{
+    [TestClass]
+    class NotificationTest
+    {
+        [TestMethod]
+        public void TestNotification()
+        {
+            string id = "555555";
+            Notification toast = new Notification();
+            toast.Create(DateTime.Now.AddMinutes(5), id);
+            Assert.AreEqual(toast.Show().Count, 1);
+            toast.Delete(id + "1");
+            Assert.AreEqual(toast.Show().Count, 1);
+            toast.Delete(id);
+            Assert.AreEqual(toast.Show().Count, 0);
+        }
+
+    }
+}
