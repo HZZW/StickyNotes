b6ad1f5b1be604218ab23072a35934723a6f9b33
HZZW <3590139958+HZZW@users.noreply.github.com>
2018-08-01 11:02:43 +0800

    Table和Tree部分单元测试

---
+++ StickyNotes.UnitTest/TextTools/TableTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StickyNotes.TextTools;
+
+namespace StickyNotes.UnitTest.TextTools
+{
+    [TestClass]
+    public class TableTest
+    {
+        [TestMethod]
+        public void TestSetTableAndGetTable()
+        {
+            var theTable = new Table();
+            theTable.SetTable(-1,-1,"负一行负一列");
+            var content1 = theTable.GetTable(-1, -1);
+            theTable.SetTable(1,2,"一行二列");
+            var content2 = theTable.GetTable(1, 2);
+            theTable.SetTable(2,2,"二行二列");
+            var content3 = theTable.GetTable(2, 2);
+            theTable.SetTable(1,2,"一行二列2");
+            var content4 = theTable.GetTable(1, 2);
+
+            var content5 = theTable.GetTable(3, 3);
+            var content6 = theTable.GetTable(3, 2);
+
+
+            Assert.AreEqual(content1, null);
+            Assert.AreEqual(content2, "一行二列");
+            Assert.AreEqual(content3, "二行二列");
+            Assert.AreEqual(content4, "一行二列2");
+            Assert.AreEqual(content5, null);
+            Assert.AreEqual(content6, null);
+        }
+        
+    }
+}
---
+++ StickyNotes.UnitTest/TextTools/TreeTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StickyNotes.TextTools;
+
+namespace StickyNotes.UnitTest.TextTools
+{
+    [TestClass]
+    public class TreeTest
+    {
+        [TestMethod]
+        public void TestAddPoint()
+        {
+            var theTree = new Tree<string>();
+
+            theTree.AddPoint(1);
+
+            theTree.AddPoint(2);
+            theTree.AddPoint(2);
+                 
+            Assert.AreEqual(true,theTree.SubTrees.ContainsKey(1));
+            Assert.AreEqual(true, theTree.SubTrees.ContainsKey(2));
+        }
+
+        [TestMethod]
+        public void TestAddPointWithContent()
+        {
+            var theTree = new Tree<string>();
+
+            theTree.AddPoint(1,"内容一");
+
+            theTree.AddPoint(2, "内容二1");
+            theTree.AddPoint(2, "内容二2");
+
+            Assert.AreEqual(true, theTree.SubTrees.ContainsKey(1));
+            Assert.AreEqual(true, theTree.SubTrees.ContainsKey(2));
+            Assert.AreEqual("内容一", theTree.SubTrees[1].Content);
+            Assert.AreEqual("内容二2", theTree.SubTrees[2].Content);
+        }
+
+        [TestMethod]
+        public void TestClear()
+        {
+            var theTree = new Tree<string>();
+            theTree.AddPoint(1, "内容一");
+
+            theTree.Clear();
+            Assert.AreEqual(0,theTree.SubTrees.Count);
+        }
+
+        [TestMethod]
+        public void TestAddPointByIndexList()
+        {
+
+            var theTree = new Tree<string>();
+
+            theTree.AddPointByIndexList(new List<int>() { 1, 2 }, "1,2");
+            theTree.AddPointByIndexList(new List<int>() { 1, 3 }, "1,3(1)");
+            theTree.AddPointByIndexList(new List<int>() { 1, 3 }, "1,3(2)");
+
+            Assert.AreEqual("1,2",theTree.SubTrees[1].SubTrees[2].Content);
+            Assert.AreEqual("1,3(2)", theTree.SubTrees[1].SubTrees[3].Content);
+        }
+
+        [TestMethod]
+        public void TestGetPointByIndexList()
+        {
+            var theTree = new Tree<string>();
+
+            var thePoint1=theTree.GetPointByIndexList(new List<int>() {1, 2});
+            Assert.AreEqual(thePoint1, null);
+
+            theTree.AddPointByIndexList(new List<int>() { 1, 2 }, "1,2");
+            var thePoint2 = theTree.GetPointByIndexList(new List<int>() { 1, 2 });
+            Assert.AreEqual(thePoint2.Content, "1,2");
+
+
+        }
+    }
+}
